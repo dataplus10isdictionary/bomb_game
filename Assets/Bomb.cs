4cfc32c344714d852bbd312dcdd1afca5ad8a94b
dataplus10isdictionary <k8519899@kadai.jp>
2021-12-09 16:46:08 +0900

    オブジェクトのドラッグ機能

--- Assets/Bomb.cs
+++ Assets/Bomb.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour{
-    private int SignBit;
+    int SignBit;
+    bool Touching;
     public float LIMIT_TIME = 5.0f;
     public float MOVE_SPEED = 0.05f;
     public float MOVE_UPPER_LIMMIT = 4.0f;
     public float MOVE_LOWER_LIMMIT = -4.0f;
     //デバッグで出力がうざいので。
     bool firstDebug = true;
+    
 
 
     void Start()
     {
         //初期値
         SignBit = 1;
+        Touching = false;
         Destroy(gameObject, LIMIT_TIME);
     }
     
@@ -23,7 +26,7 @@ public class Bomb : MonoBehaviour{
     void Update()
     {
         //生死の確認
-        if (!IsDead()) {
+        if (!IsDead() && (Touching==false) ) {
             Walk();
         }else {
             if(firstDebug) {
@@ -58,4 +61,31 @@ public class Bomb : MonoBehaviour{
     bool IsInCircle(){
         return false;
     }
+
+    public void OnMouseDrag()
+    {
+        Touching = true;
+        //Bombの位置をワールド座標からスクリーン座標に変換して、ObjectPointに格納
+        Vector3 objectPoint
+           = Camera.main.WorldToScreenPoint(transform.position);
+
+        //Bombの現在位置(マウス位置)を、pointScreenに格納
+        Vector3 pointScreen
+            = new Vector3(Input.mousePosition.x,
+                          Input.mousePosition.y,
+                          objectPoint.z);
+
+        //Bombの現在位置を、スクリーン座標からワールド座標に変換して、pointWorldに格納
+        Vector3 pointWorld = Camera.main.ScreenToWorldPoint(pointScreen);
+        pointWorld.z = transform.position.z;
+
+        //Bombの位置を、pointWorldにする
+        transform.position = pointWorld;
+    
+    }
+    public void OnMouseUp()
+    {
+        Touching = false;
+    }
+
 }
